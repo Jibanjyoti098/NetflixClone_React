c5413a7bc61561aeee8e29585d25e5e7402e951e
Jibanjyoti <jibanjyotisahoo098@gmail.com>
2025-10-26 03:53:09 +0530

    Fetch API from TMDB

--- src/components/TitleCard/TitleCard.jsx
+++ src/components/TitleCard/TitleCard.jsx
@@ -1,36 +1,52 @@
-import React, { useEffect, useRef } from 'react'
-import cards_data from "../../assets/cards/Cards_data"
+import React, { useEffect, useRef, useState } from 'react'
+import { Link } from 'react-router-dom';
+// import cards_data from "../../assets/cards/Cards_data"
 
 
-const TitleCard = ({title,category}) => {
-  
+function TitleCard({ title, category }) {
+
+  const [apiData, setApiData] = useState([]);
   // !Scroll wheel logic
-  const cardsRef = useRef()
-  const handlewheel = (event)=>{
+  const cardsRef = useRef();
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDA4N2YwMThiNTViMmUwZWJkYTdkMWI1YmM3OTlkMSIsIm5iZiI6MTc2MTQyMzE1OS4xMzQsInN1YiI6IjY4ZmQyZjM3MWQwODhkOTQ0ZGZiNmIyMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sfL3QgdxAf6ODHR-x7n43oPEeGBqwi3PIam3Cxuwk-U'
+    }
+  };
+
+  const handlewheel = (event) => {
     event.preventDefault();
-    cardsRef.current.scrollLeft += event.deltaY
-  }
-  useEffect(()=>{
-    cardsRef.current.addEventListener('wheel', handlewheel)
-  },[])
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+  useEffect(() => {
+    //! fetch data from API 
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+      .then(res => res.json())
+      .then(res => setApiData(res.results))
+      .catch(err => console.error(err));
+    // ~ Event scroll 
+    cardsRef.current.addEventListener('wheel', handlewheel);
+  }, []);
 
   return (
     <div className='mt-[50px] mb-[30px]'> {/*//&Title cards */}
-      <h2 className='mb-[8px]'>{title?title:"Popular on Netflix"}</h2>
+      <h2 className='mb-[8px]'>{title ? title : "Popular on Netflix"}</h2>
 
       {/* //&card list */}
       <div className='flex gap-[10px] overflow-x-scroll [&::-webkit-scrollbar]:hidden' ref={cardsRef}>
 
-        {cards_data.map((card, index)=>{
-          return <div className="relative min-w-[250px]" key={index}>
-            <img src={card.image} alt="" className='rounded-[4px] w-[240px] cursor-pointer'/> {/*//&Thumbnails*/}
-            <p className='absolute bottom-[10px] right-[10px]'>{card.name}</p> {/*//&Movies name*/}
-          </div>
+        {apiData.map((card, index) => {
+          return <Link to={`/player/${card.id}`} className="relative min-w-[250px]" key={index}>
+            <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} alt="" className='rounded-[4px] w-[240px] cursor-pointer' /> {/*//&Thumbnails*/}
+            <p className='absolute bottom-[10px] right-[10px] no-underline text-white'>{card.original_title}</p> {/*//&Movies name*/}
+          </Link>;
         })}
 
       </div>
     </div>
-  )
+  );
 }
 
 export default TitleCard
