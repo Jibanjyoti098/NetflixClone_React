040445347873f033744dc51d223d0240c227bfc1
Jibanjyoti <jibanjyotisahoo098@gmail.com>
2025-10-28 03:05:25 +0530

    passed toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from './pages/Home'
 import Login from "./pages/Login"
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import Register from './pages/Register'
 import LandingPage from './pages/LandingPage'
 import Player from './pages/Player'
+import { Toaster } from 'react-hot-toast'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './Firebase'
 const App = () => {
+  // !Login logic 
+  const navigate = useNavigate();
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user)=>{
+      if (user){
+        navigate("/home")
+      }else{
+        navigate("/login")
+      }
+    })
+  }, [])
+
   return (
     <div>
       <Routes>
@@ -16,7 +31,7 @@ const App = () => {
         <Route path="/player/:id" element={<Player/>}/>
         
       </Routes>
-     
+     <Toaster/>
     </div>
   )
 }
